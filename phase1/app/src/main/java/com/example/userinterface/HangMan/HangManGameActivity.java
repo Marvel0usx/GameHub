3dac2e54ee09b94f5f57e460ba9c504f5c134d49
Yipeng Rao <yvonne.rao@mail.utoronto.ca>
2019-10-25 19:15:12 -0400

    Added balloons and stcikman for HangMan (in layout)

--- phase1/app/src/main/java/com/example/userinterface/HangMan/HangManGameActivity.java
+++ phase1/app/src/main/java/com/example/userinterface/HangMan/HangManGameActivity.java
@@ -10,6 +10,10 @@ import android.view.View;
 import android.widget.ImageView;
 
 public class HangManGameActivity extends Activity {
-
+    private ImageView[] balloons; // balloon images
+    private int numLives = 6; // number of lives
+    private int keywordLen; // number of characters in current word
+    private int remainingBallon; // number of balloons left
+    private int numCorr; // number of letters correctly guessed
 
 }
