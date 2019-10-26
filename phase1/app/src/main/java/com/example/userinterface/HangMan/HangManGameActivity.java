bb5a7b70b249f211ab2f1458a69032f1564247e2
Yipeng Rao <yvonne.rao@mail.utoronto.ca>
2019-10-26 11:48:39 -0400

    added import statements to HangManGameActivity

--- phase1/app/src/main/java/com/example/userinterface/HangMan/HangManGameActivity.java
+++ phase1/app/src/main/java/com/example/userinterface/HangMan/HangManGameActivity.java
@@ -9,11 +9,20 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.view.Gravity;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+
 public class HangManGameActivity extends Activity {
     private ImageView[] balloons; // balloon images
     private int numLives = 6; // number of lives
     private int keywordLen; // number of characters in current word
     private int remainingBallon; // number of balloons left
     private int numCorr; // number of letters correctly guessed
+
 
 }
