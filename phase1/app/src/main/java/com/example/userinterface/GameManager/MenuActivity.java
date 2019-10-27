6992226d3f1fe750198247f7101a96dd016eb251
liangyo8 <yonlin.liang@mail.utoronto.ca>
2019-10-27 14:33:12 -0400

    added two more buttons to main menu

--- phase1/app/src/main/java/com/example/userinterface/GameManager/MenuActivity.java
+++ phase1/app/src/main/java/com/example/userinterface/GameManager/MenuActivity.java
@@ -8,23 +8,34 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.userinterface.GameManager.HangMan.HangManActivity;
-import com.example.userinterface.GameManager.HangMan.HangManGameActivity;
-import com.example.userinterface.MainActivity;
 import com.example.userinterface.R;
-import com.example.userinterface.SignupActivity;
 
 public class MenuActivity extends AppCompatActivity {
 
     Button btnStart;
+    Button btnResume;
+    Button btnStats;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         btnStart = findViewById(R.id.start);
+        btnResume = findViewById(R.id.Resume);
+        btnStats = findViewById(R.id.Stats);
     }
 
     public void startGame(View v){
         Intent intent = new Intent(MenuActivity.this, HangManActivity.class);
+        startActivity(intent);
+    }
+    public void Resume(View v){
+        Intent intent = new Intent(MenuActivity.this, HangManActivity.class);
+        startActivity(intent);
+    }
+
+    public void Stats(View view) {
+        Intent intent = new Intent(MenuActivity.this, Stats.class);
         startActivity(intent);
     }
 }
---
+++ phase1/app/src/main/java/com/example/userinterface/GameManager/Stats.java
@@ -0,0 +1,16 @@
+package com.example.userinterface.GameManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.userinterface.R;
+
+public class Stats extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stats);
+    }
+}
