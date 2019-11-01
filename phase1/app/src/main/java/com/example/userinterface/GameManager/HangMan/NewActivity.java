67785e5beb4a46259e0c74881e2837ae4641c14d
Yipeng Rao <yvonne.rao@mail.utoronto.ca>
2019-11-01 00:36:22 -0400

    Added a separate NewActivity class to modify methods and behaviors in order to adapt to changes such as creation of new classes (Balloon, AnswerKeyLetter, etc)

--- phase1/app/src/main/java/com/example/userinterface/GameManager/HangMan/NewActivity.java
+++ phase1/app/src/main/java/com/example/userinterface/GameManager/HangMan/NewActivity.java
@@ -3,6 +3,7 @@ package com.example.userinterface.GameManager.HangMan;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.example.userinterface.GameManager.Games;
 import com.example.userinterface.R;
@@ -10,6 +11,8 @@ import com.example.userinterface.R;
 
 public class NewActivity extends Activity {
     private Games gameManager;
+    private int remainingBalloons;
+    private ImageView[] balloons;
 
 
     @Override
@@ -21,5 +24,12 @@ public class NewActivity extends Activity {
         if (bundle != null) {
             gameManager = (Games) bundle.getSerializable("Game");
         }
+
+        balloons[0] = findViewById(R.id.ballon1); // the part right of equal sign is an image view
+        balloons[1] = findViewById(R.id.ballon2);
+        balloons[2] = findViewById(R.id.ballon3);
+        balloons[3] = findViewById(R.id.ballon4);
+        balloons[4] = findViewById(R.id.ballon5);
+        balloons[5] = findViewById(R.id.ballon6);
     }
 }
