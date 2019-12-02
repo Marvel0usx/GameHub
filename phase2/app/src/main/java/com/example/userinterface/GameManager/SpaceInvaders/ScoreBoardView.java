2382a5934e953a368dbe16aa25b5fa75009ef7cf
liangyo8 <yonlin.liang@mail.utoronto.ca>
2019-12-01 23:33:16 -0500

    minor visual fixes SI

--- phase2/app/src/main/java/com/example/userinterface/GameManager/SpaceInvaders/ScoreBoardView.java
+++ phase2/app/src/main/java/com/example/userinterface/GameManager/SpaceInvaders/ScoreBoardView.java
@@ -11,7 +11,7 @@ class ScoreBoardView {
     public void draw(Canvas canvas, Scoreboard scoreboard){
         canvas.drawText(scoreboard.getAppearance(), 250, scoreboard.getHeight() - 250, scoreboard.getPaintText());
         canvas.drawText("HP", 250, scoreboard.getHeight() - 200, scoreboard.getPaintText());
-        canvas.drawRect(320, scoreboard.getHeight() - 220, scoreboard.getLives() + 300, scoreboard.getHeight() - 200, scoreboard.getPaintText());
+        canvas.drawRect(320, scoreboard.getHeight() - 220, scoreboard.getLives(), scoreboard.getHeight() - 200, scoreboard.getPaintText());
         canvas.drawText("Stage: " + scoreboard.getLevel(), 40, 40, scoreboard.getPaintText());
     }
 }
