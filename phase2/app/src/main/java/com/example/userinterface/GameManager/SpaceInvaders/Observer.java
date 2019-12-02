8f6207963d9961ec2099cefb38357d4ec591cacc
Harry Wang <marvel0usx@outlook.com>
2019-12-01 22:07:12 -0500

    Added docs

--- phase2/app/src/main/java/com/example/userinterface/GameManager/SpaceInvaders/Observer.java
+++ phase2/app/src/main/java/com/example/userinterface/GameManager/SpaceInvaders/Observer.java
@@ -1,13 +1,27 @@
 package com.example.userinterface.GameManager.SpaceInvaders;
 
 public interface Observer {
-    // Method to update the observer, used by subject.
+    /**
+     * Method to update the observer of what has changed.
+     * Used by subjects.
+     * @param obj SpaceObject that has changed.
+     */
     void update(SpaceObject obj);
 
-    // Register subject to observe
+    /**
+     * Register an subject to observe and get update from it.
+     * @param subject subject to be observed.
+     */
     void registerSubject(Subject subject);
 
+    /**
+     * Method to unregister an subject that this observer is observing.
+     * @param subject the subject that has been observing.
+     */
     void unregisterSubject(Subject subject);
 
+    /**
+     * Method to unregister to all of the subjects.
+     */
     void unregisterAll();
 }
